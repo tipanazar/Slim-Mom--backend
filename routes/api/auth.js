a7c5cc3dc6703b7541846f5bde7966c86a1e6ed3
MakarenkoYevhenii <92297867+MakarenkoYevhenii@users.noreply.github.com>
2022-06-11 12:06:14 +0300

    сделал логику для логина

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -20,4 +20,31 @@ router.post("/register", async (req, res, next) => {
   }
 });
 
+router.post("/login", async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const result = await User.find({ email: email });
+    if (result[0].password !== password) {
+      return res.status(401).json({ message: "Email or password is wrong" });
+    }
+    const payload = {
+      id: result._id,
+    };
+    const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "10h" });
+    const findAndUpdate = await User.findByIdAndUpdate(result[0]._id, {
+      token,
+    });
+
+    res.status(200).json({
+      token: token,
+      user: {
+        email: findAndUpdate.email,
+      },
+    });
+  } catch (error) {
+    res.status(404).json(error.message);
+  }
+});
+
 module.exports = router;
