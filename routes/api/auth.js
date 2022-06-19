a97eba3be19808db5b4e72982277d1062577f7a0
MakarenkoYevhenii <92297867+MakarenkoYevhenii@users.noreply.github.com>
2022-06-19 20:08:33 +0300

    eqwe
    qweq

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -98,20 +98,20 @@ router.post("/login", async (req, res, next) => {
       if (errorMessage === "password") {
         throw createError(400, error.message);
       }
-      throw createError(400, "Use Valid Email");
+      throw createError(400, "використовуйте валідний Email");
     }
 
     const { email, password } = req.body;
     const result = await User.findOne({ email });
     if (!result) {
-      throw createError(401, "Email is wrong");
+      throw createError(401, "Email не знайдено");
     }
     if (!result.verify) {
-      throw createError(403, "Verify your email first!");
+      throw createError(403, "Верифікуйте ваш Email!");
     }
     const passwordCompare = await bcrypt.compare(password, result.password);
     if (!passwordCompare) {
-      throw createError(401, "Password is wrong");
+      throw createError(401, "Неправильний пароль");
     }
 
     const payload = {
