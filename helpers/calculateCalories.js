f84501112f0442b605e5d2e4ab160f181e22ea0c
ololosh-tralevich <karpeko76@gmail.com>
2022-06-20 12:14:22 +0100

    prettier

--- helpers/calculateCalories.js
+++ helpers/calculateCalories.js
@@ -1,6 +1,17 @@
-const calculateCalories = ({height = 0, age = 0, currentWeight = 0, desiredWeight = 0}) =>{
-    const result = Math.round(10 * currentWeight + 6.25 * height - 5 * age - 161 - 10 * (currentWeight - desiredWeight));
-    return result;
-}
+const calculateCalories = ({
+  height = 0,
+  age = 0,
+  currentWeight = 0,
+  desiredWeight = 0,
+}) => {
+  const result = Math.round(
+    10 * currentWeight +
+      6.25 * height -
+      5 * age -
+      161 -
+      10 * (currentWeight - desiredWeight)
+  );
+  return result;
+};
 
 module.exports = calculateCalories;
--- models/Product.js
+++ models/Product.js
@@ -2,38 +2,37 @@ const { Schema, model } = require("mongoose");
 const Joi = require("joi");
 
 const productSchema = Schema({
-    categories: {
-        type: Array,
-        required: true
-    },
-    weight: {
-        type: String,
-        required: true
-    },
-    title: {
-        type: Object,
-        required: true
-    },
-    calories: {
-        type: String,
-        required: true
-    },
-    groupBloodNotAllowed:{
-        type: Array,
-        required: true
-    }
+  categories: {
+    type: Array,
+    required: true,
+  },
+  weight: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: Object,
+    required: true,
+  },
+  calories: {
+    type: String,
+    required: true,
+  },
+  groupBloodNotAllowed: {
+    type: Array,
+    required: true,
+  },
 });
 
 const getDataForBMR = Joi.object({
-    height: Joi.string().required(),
-    age: Joi.string().required(),
-    currentWeight: Joi.string().required(),
-    desiredWeight: Joi.string().required(),
-    bloodType: Joi.string().required()
-})
+  height: Joi.string().required(),
+  age: Joi.string().required(),
+  currentWeight: Joi.string().required(),
+  desiredWeight: Joi.string().required(),
+  bloodType: Joi.string().required(),
+});
 
-const schemas = { getDataForBMR }
-
+const schemas = { getDataForBMR };
 
 const Product = model("products", productSchema);
 
