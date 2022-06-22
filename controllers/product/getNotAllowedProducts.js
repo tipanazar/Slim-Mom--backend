7dc5610d8ccba2d18fbcf1d904503605f286eae5
WebStudioFlip <73443296+WebStudioFlip@users.noreply.github.com>
2022-06-22 13:17:11 +0300

    update

--- controllers/product/getNotAllowedProducts.js
+++ controllers/product/getNotAllowedProducts.js
@@ -1,24 +1,29 @@
 const {Product} = require("../../models/Product");
-const { createError } = require("../../helpers");
+const { createError, calculateCalories } = require("../../helpers");
 
 const getNotAllowedProducts = async (req, res, next) => {
-  try {
-    const groupBlood = req.params.groupBlood;
-    
-    if (!([1,2,3,4].find(el=>el == groupBlood))){        
+    try {
+      const { error } = schemas.getDataForBMR.validate(req.body);
+      if (error) {
+        throw createError(400, error.message);
+      }
+      const { bloodType } = req.params;
+      const products = await (
+        await Product.find({}, "-__v")
+      ).filter((item) => item.groupBloodNotAllowed[bloodType] === true);
+      if (!products) {
         throw createError(404);
+      }
+      const calories = calculateCalories(req.body);
+  
+      const result = {
+        products: [...products].splice(0, 10),
+        calories,
+      };
+      res.json(result);
+    } catch (error) {
+      next(error);
     }
-    
-    const result = await Product.find({groupBloodNotAllowed: { $in: [true] }}); //
-    
-    if (!result) {
-      throw createError(404);
-    }
-    res.json(result);
-  } catch (error) {
-    console.log(error)
-    next(error);
-  }
-};
+  };
 
 module.exports = getNotAllowedProducts;
