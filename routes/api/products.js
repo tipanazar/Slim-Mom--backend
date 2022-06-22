fa4f6a4c762f83a75ab1a0e224e26fde2fe501ac
ololosh-tralevich <karpeko76@gmail.com>
2022-06-22 18:45:20 +0100

    Update products.js

--- routes/api/products.js
+++ routes/api/products.js
@@ -3,11 +3,12 @@ const router = express.Router();
 
 const { auth } = require("../../middlewares");
 const ctrl = require("../../controllers/product");
+const postNotAllowedProducts = require("../../controllers/user/postNotAllowedProducts");
 
 router.get("/search/:searchQuerry", ctrl.searchProducts);
 
 router.post("/bloodtype/:bloodType", ctrl.getNotAllowedProducts);
 
-router.post("/user/bloodtype/:bloodType", auth, ctrl.getNotAllowedProducts);
+router.post("/user/bloodtype/:bloodType", auth, postNotAllowedProducts);
 
 module.exports = router;
