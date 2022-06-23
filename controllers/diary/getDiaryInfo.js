bd55e7fafe1a8cdb91803a3b2072e1e5d95cb479
ololosh-tralevich <karpeko76@gmail.com>
2022-06-23 12:06:26 +0100

    ,

--- controllers/diary/getDiaryInfo.js
+++ controllers/diary/getDiaryInfo.js
@@ -1,7 +1,5 @@
 const { Diary } = require("../../models/Diary");
-const getProductsListbyId = require("../product/getProductsListbyId");
 const { createError } = require("../../helpers");
-const e = require("express");
  
 const getDiaryInfo = async (req, res, next) => {
   try {
@@ -15,7 +13,6 @@ const getDiaryInfo = async (req, res, next) => {
 
     res.status(200).json(result);
   } catch (error) {
-    console.log(error);
     next(error);
   }
 };
